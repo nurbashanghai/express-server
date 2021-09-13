7f816cf9aefa6ac11ed4df1e6257e6082ea7286c
akai1881 <akaibegaliev1881@gmail.com>
2021-09-13 13:15:24 +0600

    add comment in server js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();
 
+const studentRoutes = require('./routes/students.js');
+const todosRoutes = require('./routes/todos.js');
+
 const routes = require('./routes/index.js');
 
 const PORT = process.env.PORT;
@@ -12,6 +15,12 @@ let students = [];
 
 app.use(express.json());
 
+// ! --------- SEPARATE ROUTING SYSTEM ---------
+// app.use('/api/v1', studentsRoutes)
+// app.use('/api/v2', todosRoutes)
+// ! ---------  ROUTING SYSTEM ---------
+
+// ! UNION ROUTING SYSTEMA
 app.use('/makers', routes);
 
 app.listen(PORT, () => {
